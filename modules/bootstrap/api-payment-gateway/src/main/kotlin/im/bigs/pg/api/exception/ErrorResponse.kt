7f9b820676461a68a0b5d628486b7fb9865a9eb0
goochul-im <goochul175465@gmail.com>
2025-10-24 18:56:14 +0900

    feat(Exception): 예외처리, 예외 응답을 위한 클래스 추가
    + ErrorResponse.kt
    + ExceptionHandler.kt

---
+++ modules/bootstrap/api-payment-gateway/src/main/kotlin/im/bigs/pg/api/exception/ErrorResponse.kt
@@ -0,0 +1,55 @@
+package im.bigs.pg.api.exception
+
+import org.springframework.validation.BindingResult
+import kotlin.code
+
+class ErrorResponse(
+    errorCode: ErrorCode,
+    var errors: List<FieldError> = ArrayList()
+) {
+
+    var code:String = errorCode.code
+        private set
+
+    var message:String = errorCode.message
+        private set
+
+    class FieldError private constructor(
+        val filed:String,
+        val value:String,
+        val reason:String?
+    ){
+        companion object{
+
+            fun of(bindingResult: BindingResult): List<FieldError> {
+                val fieldErrors = bindingResult.fieldErrors
+
+                return fieldErrors.map { fieldError ->
+                    FieldError(
+                        filed = fieldError.field,
+                        value = fieldError.rejectedValue?.toString() ?: "",
+                        reason = fieldError.defaultMessage
+                    )
+                }
+            }
+
+        }
+    }
+
+    companion object {
+
+        fun of(code: ErrorCode, bindingResult: BindingResult): ErrorResponse {
+            return ErrorResponse(
+                errorCode = code,
+                errors = FieldError.of(bindingResult)
+            )
+        }
+
+        fun of(code: ErrorCode): ErrorResponse {
+            return ErrorResponse(
+                errorCode = code
+            )
+        }
+
+    }
+}
---
+++ modules/bootstrap/api-payment-gateway/src/main/kotlin/im/bigs/pg/api/exception/ExceptionHandler.kt
@@ -0,0 +1,7 @@
+package im.bigs.pg.api.exception
+
+import org.springframework.web.bind.annotation.RestControllerAdvice
+
+@RestControllerAdvice
+class ExceptionHandler {
+}
