e3afa997685f0620790dc2ec809f3444433e7947
goochul-im <goochul175465@gmail.com>
2025-10-24 20:03:04 +0900

    refactor(AES256EncryptorTest.kt): 기존 테스트 수정 및 예외 테스트 추가

--- modules/external/pg-client/src/test/kotlin/im/bigs/pg/external/pg/security/AES256EncryptorTest.kt
+++ modules/external/pg-client/src/test/kotlin/im/bigs/pg/external/pg/security/AES256EncryptorTest.kt
@@ -1,6 +1,7 @@
 package im.bigs.pg.external.pg.security
 
 import im.bigs.pg.application.pg.port.out.PgApproveRequest
+import im.bigs.pg.external.pg.exception.EncryptFailedException
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
@@ -13,18 +14,16 @@ class AES256EncryptorTest {
 
     @BeforeEach
     fun setUp() {
-        encryptor = AES256Encryptor()
+        encryptor = AES256Encryptor(
+            TEST_IV_BASE64URL = "TESTURL",
+            ALGORITHM = "AES/GCM/NoPadding",
+            API_KEY = "TESTAPIKEY"
+        )
     }
 
     @Test
     fun `Encryptor는 Base64로 정확히 암호화되어야한다`() {
-        val request = PgApproveRequest(
-            partnerId = 1L,
-            amount = BigDecimal(10000),
-            cardBin = "123456",
-            cardLast4 = "1234",
-            productName = "Test Product"
-        )
+        val request = makeRequest()
 
         val encryptedText = encryptor.encrypt(request)
 
@@ -33,4 +32,23 @@ class AES256EncryptorTest {
         assertDoesNotThrow { Base64.getUrlDecoder().decode(encryptedText) }
     }
 
+    @Test
+    fun `환경변수가 잘못 입력되면 예외가 발생한다`() {
+        val request = makeRequest()
+        encryptor = AES256Encryptor(
+            "TESTURL",
+            "BAD_ALGORITHM",
+            "TESTALGORITHM"
+        )
+
+        assertThrows(EncryptFailedException::class.java) { encryptor.encrypt(request) }
+    }
+
+    private fun makeRequest(): PgApproveRequest = PgApproveRequest(
+        partnerId = 1L,
+        amount = BigDecimal(10000),
+        cardBin = "123456",
+        cardLast4 = "1234",
+        productName = "Test Product"
+    )
 }
