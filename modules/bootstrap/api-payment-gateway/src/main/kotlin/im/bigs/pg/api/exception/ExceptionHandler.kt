8e305008b9d0067b371cc2df126042102b455cc1
goochul-im <goochul175465@gmail.com>
2025-10-25 17:57:08 +0900

    feat(ExceptionHandler): Exception 핸들링 메소드 추가
    @ ExceptionHandler.kt
    클라이언트에 에러를 그대로 반환하지 않도록 Encryption, PgClient 예외 핸들링 메소드 추가

--- modules/bootstrap/api-payment-gateway/src/main/kotlin/im/bigs/pg/api/exception/ExceptionHandler.kt
+++ modules/bootstrap/api-payment-gateway/src/main/kotlin/im/bigs/pg/api/exception/ExceptionHandler.kt
@@ -1,7 +1,30 @@
 package im.bigs.pg.api.exception
 
+import im.bigs.pg.common.exception.ErrorCode
+import im.bigs.pg.external.pg.exception.EncryptFailedException
+import im.bigs.pg.external.pg.exception.PgClientException
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
 
 @RestControllerAdvice
 class ExceptionHandler {
+
+    @ExceptionHandler(EncryptFailedException::class)
+    fun handleEncryptFailedException(e: EncryptFailedException): ResponseEntity<ErrorResponse> {
+
+        val response = ErrorResponse(ErrorCode.ENCRYPTION_ERROR)
+
+        return ResponseEntity(response, HttpStatus.BAD_REQUEST)
+    }
+
+    @ExceptionHandler(PgClientException::class)
+    fun handlePgClientException(e: PgClientException): ResponseEntity<ErrorResponse> {
+
+        val response = ErrorResponse(ErrorCode.PG_CLIENT_EXCEPTION)
+
+        return ResponseEntity(response, HttpStatus.BAD_REQUEST)
+    }
+
 }
