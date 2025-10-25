3761191f56c98feaa16e7f43b8439431dd515536
goochul-im <goochul175465@gmail.com>
2025-10-25 15:23:45 +0900

    build(external module): 코틀린 jackson 의존성 추가

--- modules/external/pg-client/build.gradle.kts
+++ modules/external/pg-client/build.gradle.kts
@@ -11,4 +11,8 @@ dependencies {
     implementation(projects.modules.domain)
     implementation(projects.modules.common)
     implementation(libs.spring.boot.starter.web)
+    testImplementation(libs.spring.boot.starter.test) {
+        exclude(module = "mockito-core")
+    }
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 }
