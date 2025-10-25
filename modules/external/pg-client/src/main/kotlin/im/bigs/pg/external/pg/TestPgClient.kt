ccc2bc006c6710e8f091a4a3f70ccac9cffcce90
goochul-im <goochul175465@gmail.com>
2025-10-25 15:25:57 +0900

    feat(TestPG): TestPG 연동 코드 완성
    @ TestPgClient.kt
    Pg 서버에 요청을 보내고, 응답에 따라 예외를 던지거나, 리턴값을 반환하도록 추가

--- modules/external/pg-client/src/main/kotlin/im/bigs/pg/external/pg/TestPgClient.kt
+++ modules/external/pg-client/src/main/kotlin/im/bigs/pg/external/pg/TestPgClient.kt
@@ -9,6 +9,7 @@ import im.bigs.pg.external.pg.dto.TestPgFailureResponse
 import im.bigs.pg.external.pg.dto.TestPgSuccessResponse
 import im.bigs.pg.external.pg.exception.PgClientException
 import im.bigs.pg.external.pg.security.PgEncryptor
+import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.http.HttpEntity
 import org.springframework.http.HttpHeaders
@@ -18,22 +19,20 @@ import org.springframework.stereotype.Component
 import org.springframework.web.client.HttpClientErrorException
 import org.springframework.web.client.HttpServerErrorException
 import org.springframework.web.client.RestTemplate
-import java.time.LocalDateTime
 import java.util.Collections
 
 @Component
 class TestPgClient(
     private val aes256Encryptor: PgEncryptor,
     private val restTemplate: RestTemplate,
+    private val objectMapper: ObjectMapper,
+    @Value("\${pgclient.test-pg.url}")
+    private var API_URL: String,
+    @Value("\${pgclient.test-pg.api-key}")
+    private var API_KEY: String
 ) : PgClientOutPort {
 
-    @Value("\${pgclient.test-pg.url}")
-    private lateinit var API_URL: String
-
-    @Value("\${pgclient.test-pg.api-key}")
-    private lateinit var API_KEY: String
-
-    private val objectMapper = ObjectMapper()
+    private val log = LoggerFactory.getLogger(javaClass)
 
     override fun supports(partnerId: Long): Boolean {
         TODO("Not yet implemented")
@@ -45,7 +44,8 @@ class TestPgClient(
         val headers = HttpHeaders()
         headers.set("Content-Type", "application/json")
         headers.set("API-KEY", API_KEY)
-        val requestEntity = HttpEntity<Unit>(headers)
+        val requestBody = Collections.singletonMap("enc", encValue)
+        val requestEntity = HttpEntity(requestBody, headers)
 
         try {
 
@@ -54,39 +54,49 @@ class TestPgClient(
                 HttpMethod.POST,
                 requestEntity,
                 TestPgSuccessResponse::class.java,
-                Collections.singletonMap("enc", encValue)
             )
 
-            return PgApproveResult( // 응답이 못 들어올 때는 어떤 데이터를 주어야 하는가??
-                approvalCode = response.body?.approvalCode ?: "THIS-PAY-IS-CANCELED",
-                approvedAt = response.body?.approvedAt ?: LocalDateTime.now(),
-                status = PaymentStatus.valueOf(response.body?.status ?: "CANCELED"),
+            val responseBody = response.body ?: run {
+                log.error("TestPg 2xx OK 응답을 받았으나, body가 null입니다.")
+                throw PgClientException("TestPG 응답 데이터가 비어있습니다.")
+            }
+
+            return PgApproveResult(
+                approvalCode = responseBody.approvalCode,
+                approvedAt = responseBody.approvedAt,
+                status = PaymentStatus.valueOf(responseBody.status),
             )
         } catch (e: HttpClientErrorException) {
             // e.statusCode (e.g., 422, 401)
             // e.responseBodyAsString (오류 JSON 문자열)
+            log.error("TestPg사와의 통신이 실패하였습니다!! (4xx) message = ${e.message}")
             val errorResponse = objectMapper.readValue(e.responseBodyAsString, TestPgFailureResponse::class.java)
             when (e.statusCode.value()) {
-                422 -> TODO()
-                401 -> TODO()
+                422 -> throw PgClientException(get422ExceptionMessage(errorResponse))
+                401 -> throw PgClientException("요청 형식에 문제가 있습니다. message = ${e.message}")
+                else -> throw PgClientException("알수 없는 에러. message = ${e.message}")
             }
 
         } catch (e: HttpServerErrorException) {
             // [5xx 오류 처리]
+            log.error("TestPg 서버에 문제가 있습니다!! (5xx) message = ${e.message}")
             throw PgClientException("TestPG 서버에 에러가 있습니다. message = ${e.message}")
+        } catch (e: Exception) {
+            log.error("PG 승인 처리 중 알 수 없는 예외가 발생하였습니다.", e)
+            throw PgClientException("PG 승인 처리 중 내부 오류 발생: ${e.message}")
         }
-
-        return TODO("반환 값을 제공하세요")
     }
 
-    private fun throw422Exception(response: TestPgFailureResponse) {
-        when (response.errorCode.toInt()) {
-            1001 -> TODO()
-            1002 -> TODO()
-            1003 -> TODO()
-            1004 -> TODO()
-            1005 -> TODO()
+    private fun get422ExceptionMessage(response: TestPgFailureResponse) : String {
+        var message = ""
+        when (response.code) {
+            1001 -> message = "도난 또는 분실된 카드입니다."
+            1002 -> message = "한도가 초과되었습니다."
+            1003 -> message = "정지되었거나 만료된 카드입니다."
+            1004 -> message = "위조 또는 변조된 카드입니다."
+            1005 -> message = "위조 또는 변조된 카드입니다.(허용되지 않은 카드)"
         }
+        return message
     }
 
 }
